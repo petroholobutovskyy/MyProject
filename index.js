c062d8864e87c369f7c8d07e5d50b677c023e173
Petro Holobutovskyy <p.holobutowski@gmail.com>
2021-08-22 23:05:55 +0300

    wstepny kod

---
+++ index.js
@@ -0,0 +1,221 @@
+// import React, {Component} from 'react';
+//
+// class Header extends Component {
+//     render() {
+//         return (
+//             <div className="container" style={{border: "1px solid blue"}}>
+//                 <div className="header__container">
+//                     <a href="#" className="header-logo">Petro<span>Travel</span></a>
+//                     <div className="header__user-container">
+//                         <div className="header-user">userIcon</div>
+//                         <div className="header-shopping-busket">Shopping Busket</div>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default Header;
+//
+// import React, {Component} from 'react';
+//
+// class HeaderMain extends Component {
+//     render() {
+//         return (
+//             <div className="container" style={{border: "1px solid yellow"}}>
+//                 <nav className="header__navigation">
+//                     <ul className="navigation-list">
+//                         <li className="navigation-list-item"><a href="#"> Strona Glowna</a></li>
+//                         <li className="navigation-list-item"><a href="#">Dlaczego my?</a></li>
+//                         <li className="navigation-list-item"><a href="#">Wycieczki</a></li>
+//                         <li className="navigation-list-item"><a href="#">Opinie</a></li>
+//                         <li className="navigation-list-item"><a href="#">Kontakt</a></li>
+//                     </ul>
+//                 </nav>
+//
+//             </div>
+//         );
+//     }
+// }
+//
+// export default HeaderMain;
+//
+// import React, {Component} from 'react';
+//
+// class MainInfo extends Component {
+//     render() {
+//         return (
+//             <div className="container" style={{border: "1px solid red"}}>
+//                 <div className="main__info-container">
+//                     <h1 className="main__info-header">Jestesmy najwiekszym biurem podrózy w regionie!</h1>
+//                     <p className="main__info-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+//                         et dolore magna aliqua.
+//                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+//                         commodo consequat.</p>
+//                     <div className="btn-div"><button className="main__info-btn">Rezerwuj online</button></div>
+//                 </div>
+//
+//             </div>
+//         );
+//     }
+// }
+//
+// export default MainInfo;
+//
+// import React, {Component} from 'react';
+//
+// class ExcursionsInfo extends Component {
+//     render() {
+//         return (
+//             <div className="container" style={{border: "1px solid green"}}>
+//                 <div className="excursion__main-container">
+//                     <div className="excursion__single-container">
+//                         <h2 className="excursion__header">Wyprawa w góry</h2>
+//                         <p className="excursion__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+//                             labore et dolore magna aliqua. </p>
+//                         <div className="excursion-buttons">
+//                             <a href="#">Wiecej</a>
+//                             <button>Rezerwuj</button>
+//                         </div>
+//                     </div>
+//                     <div className="excursion__single-container">
+//                         <h2 className="excursion__header">Poznaj niesamowity Lwów!</h2>
+//                         <p className="excursion__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+//                             labore et dolore magna aliqua. </p>
+//                         <div className="excursion-buttons">
+//                             <a href="#">Wiecej</a>
+//                             <button>Rezerwuj</button>
+//                         </div>
+//                     </div>
+//                     <div className="excursion__single-container">
+//                         <h2 className="excursion__header">Sredniowieczne zamki Galicji</h2>
+//                         <p className="excursion__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+//                             labore et dolore magna aliqua. </p>
+//                         <div className="excursion-buttons">
+//                             <a href="#">Wiecej</a>
+//                             <button>Rezerwuj</button>
+//                         </div>
+//                     </div>
+//                     <div className="excursion__single-container">
+//                         <h2 className="excursion__header">Splyw statkiem rzeka Dnister</h2>
+//                         <p className="excursion__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+//                             labore et dolore magna aliqua. </p>
+//                         <div className="excursion-buttons">
+//                             <a href="#">Wiecej</a>
+//                             <button>Rezerwuj</button>
+//                         </div>
+//                     </div>
+//                 </div>
+//
+//             </div>
+//         );
+//     }
+// }
+//
+// export default ExcursionsInfo;
+//
+// import React, {Component} from 'react';
+//
+// class WhyUs extends Component {
+//     render() {
+//         return (
+//             <div className="container" style={{border: "1px solid black"}}>
+//                 <div className="why-us__container">
+//                     <h1 className="why-us__header">Dlaczego my?</h1>
+//                     <div className="why-us-reasons">
+//                         <div className="single-reason">Reason 1</div>
+//                         <div className="single-reason">Reason 2</div>
+//                         <div className="single-reason">Reason 3</div>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default WhyUs;
+//
+// import React, {Component} from 'react';
+//
+// class Services extends Component {
+//     render() {
+//         return (
+//             <div className="container" style={{border: "1px solid grey"}}>
+//                 <h1 className="services__header">Rezerwujac z nami dostajesz:</h1>
+//                 <div className="service-container">
+//                     <img src="../images/mount.jpeg" alt="photo"/>
+//                     <p className="service__text-left">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+//                 </div>
+//                 <div className="service-container">
+//                     <p className="service__text-right">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+//                     <img src="../images/mount.jpeg" alt="photo"/>
+//                 </div>
+//                 <div className="service-container">
+//                     <img src="../images/mount.jpeg" alt="photo"/>
+//                     <p className="service__text-left">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default Services;
+//
+// import React, {Component} from 'react';
+//
+// class Form extends Component {
+//     render() {
+//         return (
+//             <div className="container" style={{border: "1px solid red"}}>
+//                 <div className="form__container">
+//                     <div className="form__header">
+//                         <h1>Rezerwuj teraz!</h1>
+//                         <p>Zarezerwuj swoja wymarzona podroz teraz i wyruszaj z nami!</p>
+//                     </div>
+//                     <form className="form">
+//                         <div className="input-single"><input type="text" name="FirstName" placeholder="First name"/></div>
+//                         <div className="input-single"> <input type="text" name="lastName" placeholder="Last name"/></div>
+//                         <div className="input-single"><input type="email" name="email" placeholder="Your email address"/></div>
+//                         <div className="input-single"><input type="tel" name="tel" placeholder="Your phone number"/></div>
+//                         <div className="input-single"><input type="tel" name="tel" placeholder="Your phone number"/></div>
+//                         <div className="input-single"><input type="number" name="numberOfPeople" placeholder="Select number of travelers"/></div>
+//                         <div className="input-single"><input type="date"/></div>
+//                         <div className="input-single"><select name="excursions" id="excursions-select">
+//                             <option value="">Wybierz wycieczke:</option>
+//                             <option value="gory">Wyprawa w góry</option>
+//                             <option value="lwow">Pozdnaj niesamowity Lwów!</option>
+//                             <option value="zamki">Sredniowieczne zamki Galicji</option>
+//                             <option value="Dnister">Splyw statkiem rzeka Dnister</option>
+//                         </select></div>
+//                         <div className="input-single"><textarea cols="24" rows="4" placeholder="Czy masz jakies pytania?"></textarea></div>
+//                         <button className="btn-form">Rezerwuj</button>
+//                     </form>
+//                 </div>
+//
+//             </div>
+//         );
+//     }
+// }
+//
+// export default Form;
+//
+// import React, {Component} from 'react';
+//
+// class Footer extends Component {
+//     render() {
+//         return (
+//             <div className="container" style={{border: "1px solid purple"}}>
+//                 <div className="footer__container">
+//                     <a href="#">Logo</a>
+//                     <div className="social-media-icons">
+//                         <div>FB Icon</div>
+//                         <div>TwitterIcon</div>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default Footer;
